047ca8d51325e6da68238ea22cffff3aab3959f0
Kalitrik <70946597+KALITRIK@users.noreply.github.com>
2022-03-21 20:58:47 +0300

    Programın Kendisi

---
+++ Piyango.py
@@ -0,0 +1,44 @@
+from random import randint
+
+bir = randint(0,1000)
+iki = randint(0,1000)
+üç = randint(0,1000)
+dört = randint(0,1000)
+beş = randint(0,1000)
+
+Tebrik = []
+IphoneX = []
+
+Tebrik.append(bir)
+Tebrik.append(iki)
+Tebrik.append(üç)
+Tebrik.append(dört)
+IphoneX.append(beş)
+
+print("Asıl ödülü kazanmak için tahmin edilmesi gereken sayılar: ", Tebrik)
+print("Asıl ödülü kazanmak için tahmin edilmesi gereken sayı: ", IphoneX)
+input("Başlamak için Enter'e Bas!")
+
+while True:
+    ab = randint(0,1000)
+    print(ab)
+    
+    if ab in Tebrik:
+        print("TEBRİKLER! Ödülüne daha da yaklaştın!")
+        input("İlerlemek için Enter'e Bas!")
+        
+        if ab == ab:
+            Tebrik.remove(ab)
+        
+    if Tebrik == []:    
+        
+        if ab in IphoneX:
+            print("Yakışır koçuma IphoneX kazandın.")
+            if ab == ab:
+                IphoneX.remove(ab)
+            
+    if (Tebrik) == []:
+        if(IphoneX) == []:
+         break
+
+input("Çıkmak için Enter'e Basınız!")
